7fb88c9f1299d4b463bca324ca14a3dfb6efa917
shylinux <shylinux@icloud.com>
2020-08-21 09:58:47 +0800

    opt some

--- src/server/server.go
+++ src/server/server.go
@@ -3,7 +3,6 @@ package server
 import (
 	ice "github.com/shylinux/icebergs"
 	"github.com/shylinux/icebergs/base/cli"
-	"github.com/shylinux/icebergs/base/gdb"
 	"github.com/shylinux/icebergs/base/tcp"
 	"github.com/shylinux/icebergs/core/code"
 	kit "github.com/shylinux/toolkits"
@@ -33,20 +32,26 @@ var Index = &ice.Context{Name: REDIS, Help: "redis",
 		ice.CTX_INIT: {Hand: func(m *ice.Message, c *ice.Context, cmd string, arg ...string) {}},
 		ice.CTX_EXIT: {Hand: func(m *ice.Message, c *ice.Context, cmd string, arg ...string) {}},
 
-		SERVER: {Name: "server port=auto auto 启动:button 编译:button 下载:button", Help: "服务器", Action: map[string]*ice.Action{
+		SERVER: {Name: "server port=auto auto 启动:button 编译:button 下载:button cmd:textarea", Help: "服务器", Action: map[string]*ice.Action{
 			"download": {Name: "download", Help: "下载", Hand: func(m *ice.Message, arg ...string) {
 				m.Cmdy(code.INSTALL, "download", m.Conf(SERVER, kit.Keys(kit.MDB_META, runtime.GOOS)))
 			}},
 			"compile": {Name: "compile", Help: "编译", Hand: func(m *ice.Message, arg ...string) {
 				name := path.Base(strings.TrimSuffix(strings.TrimSuffix(m.Conf(SERVER, kit.Keys(kit.MDB_META, runtime.GOOS)), ".tar.gz"), "zip"))
 				m.Option(cli.CMD_DIR, path.Join(m.Conf(code.INSTALL, kit.META_PATH), name))
-				m.Cmdy(cli.SYSTEM, "make", "-j4")
+				m.Cmdy(cli.SYSTEM, "make", "-j8")
 			}},
-			gdb.START: {Name: "start", Help: "启动", Hand: func(m *ice.Message, arg ...string) {
-				if m.Option(tcp.PORT) == "" {
-					m.Option(tcp.PORT, m.Cmdx(tcp.PORT, "get"))
+			"start": {Name: "start", Help: "启动", Hand: func(m *ice.Message, arg ...string) {
+				// 分配
+				port, p := "", ""
+				for {
+					port = m.Cmdx(tcp.PORT, "select", port)
+					p = path.Join(m.Conf(cli.DAEMON, kit.META_PATH), port)
+					if _, e := os.Stat(p); e != nil && os.IsNotExist(e) {
+						break
+					}
+					port = kit.Format(kit.Int(port) + 1)
 				}
-				p := path.Join(m.Conf(cli.DAEMON, kit.META_PATH), m.Option(tcp.PORT))
 				os.MkdirAll(path.Join(p, "logs"), ice.MOD_DIR)
 				os.MkdirAll(path.Join(p, "bin"), ice.MOD_DIR)
 				os.MkdirAll(p, ice.MOD_DIR)
@@ -59,27 +64,25 @@ var Index = &ice.Context{Name: REDIS, Help: "redis",
 
 				// 启动
 				m.Option(cli.CMD_DIR, p)
-				m.Cmdy(cli.DAEMON, "bin/redis-server", "--port", m.Option(tcp.PORT))
-			}},
-			gdb.STOP: {Name: "stop", Help: "停止", Hand: func(m *ice.Message, arg ...string) {
-				m.Cmdy(cli.SYSTEM, "kill", m.Option(kit.MDB_PID))
+				m.Cmdy(cli.DAEMON, "bin/redis-server", "--port", port)
 			}},
 		}, Hand: func(m *ice.Message, c *ice.Context, cmd string, arg ...string) {
-			if len(arg) == 0 {
-				m.Cmd(cli.DAEMON).Table(func(index int, value map[string]string, head []string) {
-					if strings.HasPrefix(value[kit.MDB_NAME], "bin/redis") {
-						m.Push(kit.MDB_TIME, value[kit.MDB_TIME])
-						m.Push(kit.MDB_PORT, path.Base(value[kit.MDB_DIR]))
-						m.Push(kit.MDB_STATUS, value[kit.MDB_STATUS])
-						m.Push(kit.MDB_NAME, value[kit.MDB_NAME])
-						m.Push(kit.MDB_PID, value[kit.MDB_PID])
-
-					}
-				})
-				m.PushAction("启动", "停止")
+			if len(arg) > 0 && arg[0] != "" {
+				m.Cmdy(cli.SYSTEM, "bin/redis-cli", "-p", arg[0], kit.Split(kit.Select("info", arg, 1)))
 				return
 			}
-			m.Cmdy(cli.SYSTEM, "bin/redis-cli", "-p", arg[0], "info")
+
+			m.Cmd(cli.DAEMON).Table(func(index int, value map[string]string, head []string) {
+				if strings.HasPrefix(value[kit.MDB_NAME], "bin/redis") {
+					m.Push(kit.MDB_TIME, value[kit.MDB_TIME])
+					m.Push(kit.MDB_PORT, path.Base(value[kit.MDB_DIR]))
+					m.Push(kit.MDB_DIR, value[kit.MDB_DIR])
+					m.Push(kit.MDB_STATUS, value[kit.MDB_STATUS])
+					m.Push(kit.MDB_PID, value[kit.MDB_PID])
+					m.Push(kit.MDB_NAME, value[kit.MDB_NAME])
+				}
+			})
+			m.Sort("time", "time_r")
 		}},
 		CLIENT: {Name: "client port cmd key arg 执行:button 返回:button", Help: "客户端", Hand: func(m *ice.Message, c *ice.Context, cmd string, arg ...string) {
 			if len(arg) == 0 || arg[0] == "" || arg[0] == "0" {
