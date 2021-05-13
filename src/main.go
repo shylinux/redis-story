729e3725dccd162796b2d248a20edb56a25e2691
harveyshao <harveyshao@tencent.com>
2021-05-13 15:25:36 +0800

    opt some

---
+++ src/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	ice "github.com/shylinux/icebergs"
+	_ "github.com/shylinux/icebergs/base"
+	_ "github.com/shylinux/icebergs/core"
+	_ "github.com/shylinux/icebergs/misc"
+
+	_ "github.com/shylinux/redis-story/src/client"
+	_ "github.com/shylinux/redis-story/src/server"
+)
+
+func main() { print(ice.Run()) }
