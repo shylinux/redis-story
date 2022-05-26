e71f74d8eb4ee78eb87921cfc868ff02c81ae6a0
harveyshao <harveyshao@tencent.com>
2022-05-26 14:29:01 +0800

    opt some

--- src/kafka/client.go
+++ src/kafka/client.go
@@ -1,25 +1,71 @@
 package kafka
 
 import (
-	"path"
-
 	"shylinux.com/x/ice"
-	"shylinux.com/x/icebergs/base/cli"
-	"shylinux.com/x/icebergs/base/nfs"
-	"shylinux.com/x/icebergs/base/tcp"
-	"shylinux.com/x/icebergs/core/code"
+	"shylinux.com/x/icebergs/base/mdb"
 	kit "shylinux.com/x/toolkits"
+
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+)
+
+const (
+	CLUSTER = "cluster"
+	SERVER  = "server"
+	TOPIC   = "topic"
+	GROUP   = "group"
 )
 
 type client struct {
-	ice.Code
+	ice.Zone
+	short string `data:"cluster"`
+	field string `data:"time,id,topic,text"`
 
-	list string `name:"list path auto" help:"客户端"`
+	create string `name:"create cluster server topic group" help:"创建"`
+	send   string `name:"send cluster topic=TASK_AGENT text=hello" help:"发送"`
+	list   string `name:"list cluster@key id auto send" help:"消息队列"`
 }
 
-func (c client) List(m *ice.Message, arg ...string) {
-	p := path.Join(m.Conf(code.INSTALL, kit.Keym(nfs.PATH)), kit.TrimExt(m.Conf(tcp.SERVER, kit.Keym(cli.LINUX))))
-	c.Code.System(m, p, "bin/kafka-topics.sh", "--list", "--zookeeper", "localhost:2181")
+func (s client) Create(m *ice.Message, arg ...string) {
+	s.Zone.Create(m, m.OptionSimple(CLUSTER, SERVER, TOPIC, GROUP)...)
+
+	c, e := kafka.NewConsumer(&kafka.ConfigMap{"bootstrap.servers": m.Option(SERVER), "group.id": m.Option(GROUP)})
+	m.Assert(e)
+
+	cluster, topic := m.Option(CLUSTER), m.Option(TOPIC)
+	m.Assert(c.SubscribeTopics([]string{topic}, nil))
+
+	m.Go(func() {
+		for {
+			if msg, err := c.ReadMessage(-1); !m.Warn(err, msg) {
+				s.Insert(m, CLUSTER, cluster, mdb.TIME, msg.Timestamp.Local().Format(ice.MOD_TIME), TOPIC, topic, mdb.TEXT, string(msg.Value))
+			}
+		}
+	})
+}
+
+func (s client) Send(m *ice.Message, arg ...string) {
+	msg := m.Cmd(mdb.SELECT, m.PrefixKey(), "", mdb.HASH, m.OptionSimple(CLUSTER), kit.Dict(ice.MSG_FIELDS, "time,cluster,server,topic,group"))
+
+	p, e := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": msg.Append(SERVER)})
+	m.Assert(e)
+	defer p.Close()
+	defer p.Flush(100)
+
+	topic := m.Option(TOPIC)
+	m.Assert(p.Produce(&kafka.Message{Value: []byte(m.Option(mdb.TEXT)),
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+	}, nil))
+}
+
+func (s client) List(m *ice.Message, arg ...string) {
+	switch len(kit.Slice(arg, 0, 2)) {
+	case 0:
+		m.OptionFields("time,cluster,server,topic,group")
+	case 1:
+		m.OptionPage(kit.Slice(arg, 2)...)
+		m.Action(mdb.PAGE)
+	}
+	s.Zone.List(m, kit.Slice(arg, 0, 2)...)
 }
 
 func init() { ice.CodeCtxCmd(client{}) }
--- src/kafka/server.go
+++ src/kafka/server.go
@@ -2,11 +2,11 @@ package kafka
 
 import (
 	"path"
-	"strings"
 
 	"shylinux.com/x/ice"
 	"shylinux.com/x/icebergs/base/cli"
 	"shylinux.com/x/icebergs/base/nfs"
+	"shylinux.com/x/icebergs/base/tcp"
 	"shylinux.com/x/icebergs/core/code"
 	kit "shylinux.com/x/toolkits"
 )
@@ -17,10 +17,16 @@ type server struct {
 	source string `data:"http://mirrors.tencent.com/apache/kafka/2.8.1/kafka-2.8.1-src.tgz"`
 	linux  string `data:"http://mirrors.tencent.com/apache/kafka/2.8.1/kafka_2.12-2.8.1.tgz"`
 
-	install string `name:"install" help:"安装"`
-	list    string `name:"list path auto start install download" help:"服务器"`
+	listTopic string `name:"listTopic" help:"主题列表"`
+	addTopic  string `name:"addTopic topic=TASK_AGENT" help:"添加主题"`
+
+	start string `name:"start port=10002" help:"启动"`
+	list  string `name:"list port path auto start install download" help:"服务器"`
 }
 
+func (s server) zkport(port string) string {
+	return kit.Format(kit.Int(port) + 10000)
+}
 func (s server) Download(m *ice.Message, arg ...string) {
 	s.Code.Download(m, m.Config(nfs.SOURCE), arg...)
 }
@@ -28,20 +34,58 @@ func (s server) Install(m *ice.Message, arg ...string) {
 	s.Code.Download(m, m.Config(cli.LINUX), arg...)
 }
 func (s server) Start(m *ice.Message, arg ...string) {
-	p := path.Join(m.Conf(code.INSTALL, kit.Keym(nfs.PATH)), kit.TrimExt(m.Config(cli.LINUX)))
-	s.Code.Daemon(m, p, "bin/zookeeper-server-start.sh", "config/zookeeper.properties")
-	m.Sleep3s()
-	s.Code.Daemon(m, p, "bin/kafka-server-start.sh", "config/server.properties")
+	m.Option(code.INSTALL, ice.PT)
+	m.Option(code.PREPARE, func(p string) []string {
+		port := path.Base(p)
+
+		m.Cmd(nfs.SAVE, path.Join(p, "config/zookeeper.properties"), kit.Format(`
+clientPort=%s
+dataDir=%s
+maxClientCnxns=0
+admin.enableServer=false
+`, s.zkport(port), kit.Path(p, "var/zookeeper")))
+
+		m.Cmd(nfs.SAVE, path.Join(p, "config/server.properties"), kit.Format(`
+zookeeper.connect=localhost:%s
+listeners=PLAINTEXT://:%s
+log.dirs=%s
+broker.id=1
+num.partitions=1
+
+num.network.threads=3
+num.io.threads=8
+
+socket.send.buffer.bytes=102400
+socket.receive.buffer.bytes=102400
+socket.request.max.bytes=104857600
+num.recovery.threads.per.data.dir=1
+offsets.topic.replication.factor=1
+transaction.state.log.replication.factor=1
+transaction.state.log.min.isr=1
+
+log.retention.hours=168
+log.segment.bytes=1073741824
+
+log.retention.check.interval.ms=300000
+zookeeper.connection.timeout.ms=18000
+group.initial.rebalance.delay.ms=0
+`, s.zkport(port), port, kit.Path(p, "var/kafka-logs")))
+
+		s.Code.Daemon(m, p, "bin/zookeeper-server-start.sh", "config/zookeeper.properties")
+		return []string{}
+	})
+	s.Code.Start(m, m.Config(cli.LINUX), "bin/kafka-server-start.sh", "config/server.properties")
+}
+func (s server) ListTopic(m *ice.Message, arg ...string) {
+	s.Code.System(m, m.Option(nfs.DIR), kit.Format("bin/kafka-topics.sh --list --zookeeper localhost:%s", s.zkport(m.Option(tcp.PORT))))
+}
+func (s server) AddTopic(m *ice.Message, arg ...string) {
+	s.Code.System(m, m.Option(nfs.DIR), kit.Format("bin/kafka-topics.sh --create --zookeeper localhost:%s --replication-factor 1 --partitions 1 --topic %s", s.zkport(m.Option(tcp.PORT)), m.Option(TOPIC)))
 }
 func (s server) List(m *ice.Message, arg ...string) {
-	m.OptionFields("time,status,pid,cmd,dir")
-	m.Debug("what %v", 123)
-	m.Cmd(cli.DAEMON).Table(func(index int, value map[string]string, head []string) {
-		m.Debug("what %v", 123)
-		if strings.Contains(value[ice.CMD], "bin/kafka") || strings.Contains(value[ice.CMD], "bin/zookeeper") {
-			m.Push("", value, head)
-		}
-	})
+	if s.Code.List(m, "kafka-server", arg...); len(arg) == 0 {
+		m.PushAction(s.ListTopic, s.AddTopic)
+	}
 }
 
 func init() { ice.CodeCtxCmd(server{}) }
