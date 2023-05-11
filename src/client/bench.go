101225a82f638a044bd9d523a2cea8dc04fd5a57
shylinux <shylinux@163.com>
2023-05-11 13:46:49 +0800

    opt some

---
+++ src/client/bench.go
@@ -0,0 +1,88 @@
+package client
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"sync/atomic"
+	"time"
+
+	"shylinux.com/x/ice"
+	"shylinux.com/x/toolkits/conf"
+	"shylinux.com/x/toolkits/logs"
+	"shylinux.com/x/toolkits/task"
+)
+
+type Stat struct {
+	NReq int64
+	NErr int64
+	NOK  int64
+
+	NRead  int64
+	NWrite int64
+
+	BeginTime time.Time
+	EndTime   time.Time
+
+	Cost time.Duration
+
+	Up   float64
+	Down float64
+	QPS  float64
+	AVG  time.Duration
+}
+
+var trans = map[string]func(i int64) []string{
+	"GET": func(i int64) []string { return []string{fmt.Sprintf("hi%d", i)} },
+	"SET": func(i int64) []string { return []string{fmt.Sprintf("hi%d", i), "hello"} },
+}
+var ErrNotFoundCmd = errors.New("not found cmd")
+
+func Bench(nconn, nreq int64, hosts []string, cmds []string, check func(cmd string, arg []string, res ice.Any)) (*Stat, error) {
+	s := &Stat{BeginTime: time.Now()}
+	defer func() {
+		if s.EndTime = time.Now(); s.BeginTime != s.EndTime {
+			d := float64(s.EndTime.Sub(s.BeginTime)) / float64(time.Second)
+			s.Up = float64(s.NWrite) / d
+			s.Down = float64(s.NRead) / d
+			s.QPS = float64(s.NReq) / d
+			s.AVG = s.EndTime.Sub(s.BeginTime) / time.Duration(nreq)
+		}
+	}()
+	rp := NewRedisPool(hosts[0], "")
+	tp := task.New(conf.Sub(task.TASK))
+	defer tp.Close()
+	tp.WaitN(int(nconn), func(task *task.Task, lock *task.Lock) error {
+		var nerr, nok int64
+		defer func() {
+			atomic.AddInt64(&s.NReq, nreq)
+			atomic.AddInt64(&s.NErr, nerr)
+			atomic.AddInt64(&s.NOK, nok)
+		}()
+		conn := rp.Get()
+		defer rp.Put(conn)
+		cmd := strings.ToUpper(cmds[0])
+		method := trans[cmd]
+		if method == nil {
+			return ErrNotFoundCmd
+		}
+		for i := int64(0); i < nreq; i++ {
+			func() {
+				defer logs.CostTime(func(d time.Duration) {
+					defer lock.Lock()()
+					s.Cost += d
+				})()
+				arg := method(i)
+				if reply, err := conn.Do(cmd, arg...); err != nil {
+					nerr++
+				} else {
+					if nok++; check != nil {
+						check(cmd, arg, reply)
+					}
+				}
+			}()
+		}
+		return nil
+	})
+	return s, nil
+}
