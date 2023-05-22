43677395f6417bf3bf03264332fa046fc846e61e
shylinux <shylinux@163.com>
2023-05-22 22:24:11 +0800

    add some

--- etc/miss.sh
+++ etc/miss.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ -f $PWD/.ish/plug.sh ]; then source $PWD/.ish/plug.sh; elif [ -f $HOME/.ish/plug.sh ]; then source $HOME/.ish/plug.sh; else
 	ctx_temp=$(mktemp); if curl -h &>/dev/null; then curl -o $ctx_temp -fsSL https://shylinux.com; else wget -O $ctx_temp -q http://shylinux.com; fi; source $ctx_temp intshell
@@ -7,7 +7,7 @@ fi
 require miss.sh
 ish_miss_prepare_compile
 ish_miss_prepare_develop
-ish_miss_prepare_operate
+ish_miss_prepare_project
 
 ish_miss_prepare release
 ish_miss_prepare_icebergs
