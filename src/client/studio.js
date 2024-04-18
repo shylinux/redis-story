f43fa3ca4213a1169bcbd7a7dc3c2368a5156eb9
shy <shy@shylinux.com>
2024-04-18 10:48:42 +0800

    add some

--- src/client/studio.js
+++ src/client/studio.js
@@ -2,7 +2,7 @@ Volcanos(chat.ONIMPORT, {
 	_init: function(can, msg) { can.require(["/plugin/story/studiolayout.js"], function() {
 		can.onimport.project(can, msg, aaa.SESS, function(event, sess, value) { return {
 			content: {index: "web.code.redis.keys", args: sess},
-			profile: {index: "web.code.redis.commands", args: sess},
+			profile: can.isCmdMode() && {index: "web.code.redis.commands", args: sess},
 			display: {index: "web.code.redis.shells", args: sess},
 		} })
 	}) },
