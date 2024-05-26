5810eae40cc694cf9a79973856c64a373c8c2b7e
shy <shy@shylinux.com>
2024-05-26 11:28:20 +0800

    add some

--- src/client/studio.go
+++ src/client/studio.go
@@ -3,6 +3,7 @@ package client
 import (
 	"shylinux.com/x/ice"
 	"shylinux.com/x/icebergs/base/aaa"
+	"shylinux.com/x/icebergs/base/web"
 	kit "shylinux.com/x/toolkits"
 )
 
@@ -14,6 +15,11 @@ type studio struct {
 	list   string `name:"list list" icon:"redis.png"`
 }
 
+func (s studio) Init(m *ice.Message, arg ...string) {
+	web.AddPortalProduct(m.Message, "Redis Studio", `
+一款网页版的 Redis 工作台，用来下载安装 Redis、组建集群、操作数据等。
+`, 10.0)
+}
 func (s studio) Create(m *ice.Message, arg ...string) { m.Cmdy(s.Client, m.ActionKey(), arg) }
 func (s studio) Remove(m *ice.Message, arg ...string) { m.Cmdy(s.Client, m.ActionKey(), arg) }
 func (s studio) List(m *ice.Message, arg ...string) {
